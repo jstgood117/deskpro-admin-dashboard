488a3e5d4c638853040159b11d9cfd7f8d86d519
Paul <paul@modezero.net>
2019-10-12 18:33:22 +0100

    sessionStorage for API URL

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import React, { SFC } from 'react';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
 
-import { dataSource } from './resources/constants';
+//import { dataSource } from './resources/constants';
 import Router from './components/Router';
 
+const apiUrl = window.sessionStorage.getItem('DESKPRO_ADMIN_API_URL');
+console.log("API URL: " + apiUrl);
+
 const client = new ApolloClient({
-  uri: dataSource,
+//  uri: dataSource,
+uri: apiUrl,
 });
+
 
 const App: SFC = () => (
 	<ApolloProvider client={client}>
