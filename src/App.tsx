5eca14ea6ab442971785db5e5dd1b14d5b6bd1a8
Christopher Nadeau <chris@nadeau.tv>
2019-10-16 12:02:11 +0100

    Add logging

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
 // import { ApolloProvider, useQuery } from '@apollo/react-hooks';
 import {IntlProvider} from 'react-intl';
+import { appDebug } from './logging';
 
 import { testInitialData } from './resources/constants';
 import Router from './components/Router';
 // import { QUERY_INITIAL } from './resources/graphql';
 
 const apiUrl = window.sessionStorage.getItem('DESKPRO_ADMIN_API_URL');
-console.log("API URL: " + apiUrl);
+appDebug("API URL: " + apiUrl);
 
 const client = new ApolloClient({
 	uri: apiUrl,
