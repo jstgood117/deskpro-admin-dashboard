c74de4f6587348b8ff9264e83a9a34052c9c9671
webdatavisualdev <cli698638@gmail.com>
2019-10-17 11:54:52 -0400

    make transparent border for text avatar

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,7 +3,6 @@ import { configure, addDecorator } from '@storybook/react';
 import { ThemeProvider } from 'styled-components';
 
 import { DeskproAdminTheme } from '../src/components/Theme';
-import Main from '../src/components/Main';
 
 const req = require.context('../src/components', true, /.stories.tsx$/);
 
@@ -13,9 +12,7 @@ function loadStories() {
 
 addDecorator((story) => (
   <ThemeProvider theme={DeskproAdminTheme}>
-    <Main>
-      {story()}
-    </Main>
+    {story()}
   </ThemeProvider>
 ))
 
