91e52baec0e95b0df8ccd96ab9350b1bb95e7f8b
Christopher Nadeau <chris@nadeau.tv>
2019-10-25 16:39:01 +0100

    cleanup a bit

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,22 @@
 import React, { SFC } from 'react';
 import { useQuery } from '@apollo/react-hooks';
-import {IntlProvider} from 'react-intl';
+import { IntlProvider } from 'react-intl';
+import { flatMap } from 'lodash';
 
-// import { testInitialData } from './resources/constants';
-//import Loading from './components/Loading';
-//import Error from './components/Error';
 import { logError } from './components/Error/ErrorBoundary';
 import { HashRouter, Switch, Route } from 'react-router-dom';
 import { gql } from 'apollo-boost';
 import { SidebarContainer, AppContainer, BodyContainer } from './pages/AdminInterface';
 import Sidebar from './components/Sidebar';
-import { ISidebarItem, ISidebarSection } from './resources/interfaces';
+import { ISidebarSection } from './resources/interfaces';
 import PageType from './components/Page/PageType';
 
-const arrayToObject = (array: Array<any>) =>
-	array.reduce((obj, item) => {
-		obj[item.id] = item.message;
-		return obj
-}, {})
-
-const collectNavItems = (sects: ISidebarSection[]): ISidebarItem[] => {
-	const all: ISidebarItem[] = [];
-
-	sects.forEach(section => {
-		if (section.navItems) {
-			section.navItems.forEach(i => {
-				all.push(i);
-				if (i.navItems) {
-					i.navItems.forEach(ii => {
-						all.push(ii);
-					});
-				}
-			});
-		}
-	});
-
-	return all;
-};
-
 export const QUERY_INITIAL = gql`
-  query  {
-    translations: adminInterface_getTranslations(locale: "en") {
-      id
-      message
-    }
+query  {
+	translations: adminInterface_getTranslations(locale: "en") {
+		id
+		message
+	}
 
 	user: adminInterface_getAdminUser {
 		locale
@@ -51,52 +24,64 @@ export const QUERY_INITIAL = gql`
 
 	sidebar: adminInterface_getAdminSidebar {
 		sectionName
-        icon
-        navItems {
-          itemName
-          path
-          pageType
-          metadataQuery
-          navItems {
-            itemName
-            path
-            pageType
-            metadataQuery
-          }
-        }
+		icon
+		navItems {
+			itemName
+			path
+			pageType
+			metadataQuery
+			navItems {
+				itemName
+				path
+				pageType
+				metadataQuery
+			}
+		}
 	}
-  }
+}
 `;
 
 const App: SFC = () => {
 	const locale = navigator.language;
 	console.log(`locale: ${locale}`)
-	const { loading, error, data } = useQuery(QUERY_INITIAL, { errorPolicy: 'all', variables: { locale: locale }});
-	let translations;
+	const { loading, error, data } = useQuery(QUERY_INITIAL, { errorPolicy: 'all', variables: { locale: locale } });
 
-	// Need to convert incoming translations from array of objects to single object
-	if (data) {
-		translations = arrayToObject(data.translations);
+	if (loading) {
+		return <p>Loading</p>;
+	}
+	if (error) {
+		return <p>Error: {error}</p>;
 	}
 
-  return (
+	return (
 		<HashRouter>
-			{loading && <div>Loading...</div>}
-			{error && <div>apolloError={error}</div>}
-			{data && <IntlProvider locale={data.user.locale} messages={translations} onError={(err) => {logError(err)}} >
-			<AppContainer>
-				<SidebarContainer>
-					<Sidebar data={data.sidebar} />
-				</SidebarContainer>
-				<BodyContainer>
-					<Switch>
-						{collectNavItems(data.sidebar).map((sbObj, idx) =>
-							sbObj.path && <Route key={idx} exact path={sbObj.path} render={() => <PageType {...sbObj} />} />
-						)}
-					</Switch>
-				</BodyContainer>
-			</AppContainer>
-			</IntlProvider>}
+			<IntlProvider
+				locale={data.user.locale}
+				messages={(data.translations as any[]).reduce((obj, item) => {
+					// turns [{id, msg}] array into {id: msg} map
+					obj[item.id] = item.message;
+					return obj
+				}, {})}
+				onError={(err) => { logError(err) }}
+			>
+				<AppContainer>
+					<SidebarContainer>
+						<Sidebar data={data.sidebar} />
+					</SidebarContainer>
+					<BodyContainer>
+						<Switch>
+							{
+								flatMap(
+									(data.sidebar as ISidebarSection[]).map(section => section.navItems),
+									sectionItem => flatMap(sectionItem, ss => ss.navItems || []).concat(sectionItem)
+								)
+									.filter(s => s.path)
+									.map(s => <Route key={s.path} exact path={s.path} render={() => <PageType {...s} />} />)
+							}
+						</Switch>
+					</BodyContainer>
+				</AppContainer>
+			</IntlProvider>
 		</HashRouter>
 	);
 }
