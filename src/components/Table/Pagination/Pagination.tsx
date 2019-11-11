078eea3b7be57cd866f597bf82e70df9348a1d57
jstgood117 <jstgood117@gmail.com>
2019-11-11 17:37:37 +0100

    Removed "Go to Page from Table pagination"

--- src/components/Table/Pagination/Pagination.tsx
+++ src/components/Table/Pagination/Pagination.tsx
@@ -48,18 +48,6 @@ const Pagination: SFC<IProps> = ({
         {pageIndex + 1} of {pageOptions.length}
       </strong>{' '}
     </span>
-    <span>
-      | Go to page:{' '}
-      <input
-        type='number'
-        defaultValue={pageIndex + 1}
-        onChange={e => {
-          const page = e.target.value ? Number(e.target.value) - 1 : 0;
-          gotoPage(page);
-        }}
-        style={{ width: '100px' }}
-      />
-    </span>{' '}
     <select
       value={pageSize}
       onChange={e => {
