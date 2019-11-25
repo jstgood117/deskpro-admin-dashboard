dad1630ea88fec3360a48ddf14f1e86118b7a4ad
jstgood117 <jstgood117@gmail.com>
2019-11-25 08:19:27 +0100

    removed unused story

--- src/components/Header/stories.tsx
+++ src/components/Header/stories.tsx
@@ -6,58 +6,30 @@ import { action } from '@storybook/addon-actions';
 import Header from './Header';
 import { testTranslations } from '../../resources/constants/constants';
 
-storiesOf('Header', module)
-  .add('with text', () => (
-    <IntlProvider locale='en' messages={testTranslations}>
-      <Header
-        illustration='agents-header'
-        title='admin_agents.agents.title'
-        description='admin_agents.agents.description'
-        defaulViewMode='table'
-        showViewModeSwitcher={true}
-        showNewButton={true}
-        showHelpButton={true}
-        links={[
-          {
-            title: 'LoginLog',
-            icon: 'loginLog',
-            path: '/login'
-          },
-          {
-            title: 'Settings',
-            icon: 'settings',
-            path: '/settings'
-          }
-        ]}
-        onChangeView={action('clicked onChangeView')}
-        onNewClick={action('clicked onNewClick')}
-      />
-    </IntlProvider>
-  ))
-  .add('with table actions', () => (
-    <IntlProvider locale='en' messages={testTranslations}>
-      <Header
-        illustration='agents-header'
-        title='admin_agents.agents.title'
-        description='admin_agents.agents.description'
-        defaulViewMode='table'
-        showViewModeSwitcher={true}
-        showNewButton={true}
-        showHelpButton={true}
-        links={[
-          {
-            title: 'LoginLog',
-            icon: 'loginLog',
-            path: '/login'
-          },
-          {
-            title: 'Settings',
-            icon: 'settings',
-            path: '/settings'
-          }
-        ]}
-        onChangeView={action('clicked onChangeView')}
-        onNewClick={action('clicked onNewClick')}
-      />
-    </IntlProvider>
-  ));
+storiesOf('Header', module).add('with text', () => (
+  <IntlProvider locale='en' messages={testTranslations}>
+    <Header
+      illustration='agents-header'
+      title='admin_agents.agents.title'
+      description='admin_agents.agents.description'
+      defaulViewMode='table'
+      showViewModeSwitcher={true}
+      showNewButton={true}
+      showHelpButton={true}
+      links={[
+        {
+          title: 'LoginLog',
+          icon: 'loginLog',
+          path: '/login'
+        },
+        {
+          title: 'Settings',
+          icon: 'settings',
+          path: '/settings'
+        }
+      ]}
+      onChangeView={action('clicked onChangeView')}
+      onNewClick={action('clicked onNewClick')}
+    />
+  </IntlProvider>
+));
