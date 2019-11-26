5c6d16de6400ca798ec69b2bcf29256701fee09a
jstgood117 <jstgood117@gmail.com>
2019-11-26 16:54:59 +0100

    fixing sidebar padding

--- src/components/Sidebar/SidebarSection/SidebarItem/SidebarItem.tsx
+++ src/components/Sidebar/SidebarSection/SidebarItem/SidebarItem.tsx
@@ -9,25 +9,26 @@ interface IStyleProps {
 }
 
 const SidebarItemStyled = styled(NavLink)<IStyleProps>`
-	padding: 10px 10px 10px 39px;
-	color: ${props => props.theme.staticColour};
-	text-decoration: none;
-	margin: 0;
-	overflow: hidden;
-	display: block;
+  padding: 6px 10px 6px 44px;
+  font-size: 14px;
+  color: ${props => props.theme.staticColour};
+  text-decoration: none;
+  margin: 0;
+  overflow: hidden;
+  display: block;
 
-	&:hover {
-		background-color: #d4dbdf;
-		cursor: pointer;
-	}
+  &:hover {
+    background-color: #d4dbdf;
+    cursor: pointer;
+  }
 `;
 
 const ItemName = styled(TextLabel)`
-	line-height: 100%;
-	${SidebarItemStyled}.active & {
-		color: ${props => props.theme.brandPrimary};
-		font-weight: bold;
-	}
+  line-height: 100%;
+  ${SidebarItemStyled}.active & {
+    color: ${props => props.theme.brandPrimary};
+    font-weight: bold;
+  }
 `;
 
 export interface IProps {
@@ -36,15 +37,22 @@ export interface IProps {
   depth?: number;
 }
 
-const SidebarItem: SFC<IProps> = ({path, itemName, depth}) => {
+const SidebarItem: SFC<IProps> = ({ path, itemName, depth }) => {
   const style: React.CSSProperties = {};
   if (depth) {
-    style.paddingLeft = (39 + ((depth||0)*15)) + 'px';
+    style.paddingLeft = 44 + (depth || 0) * 15 + 'px';
   }
 
   return (
-    <SidebarItemStyled to={path || '/'} exact={true} activeClassName='active' style={style}>
-      <ItemName><FormattedMessage id={itemName} /></ItemName>
+    <SidebarItemStyled
+      to={path || '/'}
+      exact={true}
+      activeClassName='active'
+      style={style}
+    >
+      <ItemName>
+        <FormattedMessage id={itemName} />
+      </ItemName>
     </SidebarItemStyled>
   );
 };
