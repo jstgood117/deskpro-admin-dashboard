7af685d7ba28e3181f13499a88922004378736cd
jstgood117 <jstgood117@gmail.com>
2019-12-05 04:16:09 +0100

    fixed table story issue

--- src/components/Table/stories.tsx
+++ src/components/Table/stories.tsx
@@ -1,19 +1,66 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { IntlProvider } from 'react-intl';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient, InMemoryCache } from 'apollo-boost';
+import { createHttpLink } from 'apollo-link-http';
 
-import Table from './Table';
+import generateConfig, { ConfigType } from '../../config/config';
+import testColumnData2 from '../../resources/constants/mock/testTableColumns2';
+import Table from './TableWrapper';
+import { testTranslations } from '../../resources/constants/constants';
+import { FilterType } from '../../services/filters/types';
 import {
-  testTableColumns,
-  testTableData
-} from '../../resources/constants/constants';
+  StandardTableProvider,
+  StandardTableContextValues
+} from '../../contexts/StandardTableContext';
 
+const tableData = testColumnData2;
+const { views } = (tableData as any)['agents_getAgentsPage'];
+const columnOrder = [
+  { column: 'agents.col.name', show: true },
+  { column: 'agents.col.email', show: true },
+  { column: 'agents.col.phone', show: true },
+  { column: 'agents.col.access', show: true },
+  { column: 'agents.col.teams', show: true },
+  { column: 'agents.col.groups', show: true }
+];
+const initialColumnOrder = columnOrder;
+const filters: FilterType[] = [];
+const tableDef = views[0].tableDef;
+const filterDef = views[0].filterDef;
+const path = '/agents';
+const contextValue: StandardTableContextValues = {
+  path,
+  filters,
+  onFilterChange: () => {},
+  onSearchChange: () => {},
+  tableDef,
+  filterDef,
+  initialColumnOrder
+};
+
+const config: ConfigType = generateConfig();
+const { apiUrl } = config;
+const link = createHttpLink({ uri: apiUrl });
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link
+});
 storiesOf('Table', module).add('with dummy data', () => (
-  <Table
-    path='/agents'
-    data={testTableData}
-    columns={testTableColumns}
-    fetchData={() => false}
-    pageCount={100}
-    tableType='sync'
-  />
+  <ApolloProvider client={client}>
+    <IntlProvider locale='en' messages={testTranslations}>
+      <StandardTableProvider value={contextValue}>
+        {views && views[0] && (
+          <Table
+            {...views[0]}
+            path='/agents' // TODO: When hooked up to live db, not required
+            filters={filters}
+            dataType='async'
+            columnOrder={columnOrder}
+          />
+        )}
+      </StandardTableProvider>
+    </IntlProvider>
+  </ApolloProvider>
 ));
