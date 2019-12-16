7af5126bdf47bbe971883aabc3f921d93700b10a
jstgood117 <jstgood117@gmail.com>
2019-12-16 18:49:43 +0100

    Updated storybook webpack config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,13 +1,25 @@
-const path = require("path");
+const path = require('path');
 const SRC_PATH = path.join(__dirname, '../src');
 
-module.exports = ({config}) => {
+module.exports = ({ config }) => {
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
     loader: require.resolve('babel-loader'),
     options: {
-      presets: [['react-app', { flow: false, typescript: true }]],
-    },
+      presets: [['react-app', { flow: false, typescript: true }]]
+    }
+  });
+  config.module.rules.push({
+    test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+    use: [
+      {
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: 'fonts/'
+        }
+      }
+    ]
   });
   config.resolve.extensions.push('.ts', '.tsx');
   return config;
