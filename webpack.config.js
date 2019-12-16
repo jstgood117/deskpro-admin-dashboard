456c0a168e7f67c36d0b4b9bfe314e1b4f027973
jstgood117 <jstgood117@gmail.com>
2019-12-16 17:41:32 +0100

    updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -7,8 +7,16 @@ module.exports = {
         loader: 'graphql-tag/loader'
       },
       {
-        test: /\.(gif|svg|jpg|png|ttf|eot|woff(2)?)(\?[a-z0-9=&.]+)?$/,
-        loader: 'url-loader?limit=100000&name=fonts/[name].[ext]'
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
       }
     ]
   }
