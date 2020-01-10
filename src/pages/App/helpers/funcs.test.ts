539656734f0083db6a5801f3cbe447041a5b1d00
Richard Fernandez <richardhealy81@gmail.com>
2020-01-10 14:31:23 +0800

    Drawer items coming directly from sidebar api data
    - Moved to live data
    - WIP: Refactored generateDrawerRoutes and generatePageRoutes to
    	accept a router generator function. These 2 functions could
    	probably be merge, or at least speed up to not make
    	(generateDrawerRoutes) O(n2)
    - Updated tests to  reflect changes

--- src/pages/App/helpers/funcs.test.ts
+++ src/pages/App/helpers/funcs.test.ts
@@ -1,37 +1,81 @@
-import { generatePageRoutes } from './funcs';
+import {
+  generatePageRoute,
+  generateDrawerRoute,
+  generatePageRoutes,
+  generateDrawerRoutes,
+} from './funcs';
 
-describe('Routes', () => {
+describe('App helper functions', () => {
+
+  const drawerItem = {
+    'itemName':'admin_sidebar.item.edit',
+    'path':'/edit',
+    'pageType':'EditAgentForm',
+    'metadataQuery':'',
+    '__typename':'SidebarItem',
+  };
+
+  const navItem = {
+    'itemName':'admin_nav.item.dashboard',
+    'path':'/',
+    'pageType':'',
+    'metadataQuery':'',
+    '__typename':'SidebarItem',
+    'drawerItems': [drawerItem]
+  };
+
+  const siderbarLinks = [
+    {
+      'sectionName':'admin_nav.section.setup',
+      'icon':'setup','navItems':[
+        navItem
+      ]
+  }];
+
+
   describe('generatePageRoutes', () => {
 
-    const siderbarLinks = [
-      {
-        'sectionName':'admin_nav.section.setup',
-        'icon':'setup','navItems':[
-        {
-          'itemName':'admin_nav.item.dashboard',
-          'path':'/',
-          'pageType':'',
-          'metadataQuery':'',
-          '__typename':'SidebarItem',
-          'drawerItems': [
-            {
-              'itemName':'admin_sidebar.item.edit',
-              'path':'/edit',
-              'pageType':'EditAgentForm',
-              'metadataQuery':'',
-              '__typename':'SidebarItem',
-            }
-          ]
-        }
-      ]
-    }];
+    test('calls IoC param function with array of drawerItem paths ', () => {
+      const mockGenerateFunc = jest.fn();
+      generatePageRoutes(siderbarLinks, mockGenerateFunc);
 
-    test('given an array of sidebar links, generates routes', () => {
-      const routes = generatePageRoutes(siderbarLinks);
+      expect(mockGenerateFunc).toHaveBeenCalledTimes(1);
+      expect(mockGenerateFunc).toHaveBeenCalledWith(navItem, ['/edit']);
+    });
+  });
 
-      expect(routes).toHaveLength(1);
-      expect(routes[0].props.path).toEqual(['/', '//edit']);
+  describe('generateDrawerRoutes', () => {
+
+    test('calls IoC param function with array of drawerItem paths ', () => {
+      const mockGenerateFunc = jest.fn();
+      generateDrawerRoutes(siderbarLinks, mockGenerateFunc);
+
+      expect(mockGenerateFunc).toHaveBeenCalledTimes(1);
+      expect(mockGenerateFunc).toHaveBeenCalledWith(drawerItem);
+    });
+  });
+
+  describe('generatePageRoute', () => {
+
+    test('given an array of sidebar links, generates page routes', () => {
+      const route = generatePageRoute(navItem);
+      expect(route.props.path).toEqual(['/']);
     });
 
+    test('given an array of sidebar links, generates page routes and extends path with drawerItem paths', () => {
+
+      const postFixPaths = ['/edit', '/test'];
+      const routeWithPostFixPaths = generatePageRoute(navItem, postFixPaths);
+      expect(routeWithPostFixPaths.props.path).toEqual(['/', '/edit', '/test']);
+    });
+
+  });
+
+  describe('generateDrawerRoute', () => {
+
+    test('given an array of sidebar links, generates drawer routes', () => {
+      const route = generateDrawerRoute(navItem);
+      expect(route.props.path).toEqual(['/']);
+    });
   });
 });
