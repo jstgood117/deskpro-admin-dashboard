dacbe7f8c6ec055e51d70578668079812f6fff92
alex-mazuruk <alexander.mazuruk.upwork@gmail.com>
2020-01-14 11:18:39 +0100

    Add story for testing drawer animation

---
+++ src/components/Drawer/stories.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { storiesOf } from '@storybook/react';
+
+import Button from '../Button';
+import Drawer from './Drawer';
+
+storiesOf('Drawer', module)
+  .add('pull right', () => {
+    const [open, setOpen] = useState(false);
+
+    const onClose = () => {
+      setOpen(false);
+    };
+
+    return (
+      <div>
+        <Drawer open={open} onClose={onClose}>
+          <div>Drawer</div>
+        </Drawer>
+
+        <Button
+          styleType='secondary'
+          onClick={() => setOpen(true)}
+          size='medium'
+        >
+          Open
+        </Button>
+      </div>
+    );
+  });
