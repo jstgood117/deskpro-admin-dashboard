962d4a500a4d91e4a990d452e38aeb1a048d162b
pxzdev <fortunewillsmileonme117@gmail.com>
2020-01-17 08:42:14 +0300

    feature/7092-string-list-builder
    Fix StringListBuilder storybook to use Formik package

--- src/components/StringListBuilder/stories.tsx
+++ src/components/StringListBuilder/stories.tsx
@@ -2,30 +2,52 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import StringListBuilder from './StringListBuilder';
+import { Formik } from 'formik';
+import { action } from '@storybook/addon-actions';
 
 storiesOf('String List Builder', module)
   .add('List builder uncapped', () => (
-    <StringListBuilder
-      addTitle='Add IP'
-      showTitle={true}
-      title='Whitelisted IPs'
-      values={['test']}
-    />
+    <Formik
+      initialValues={{ test: ['test'] }}
+      validate={action('validate')}
+      onSubmit={action('submit')}
+    >
+      {props => (
+        <StringListBuilder
+          addTitle='Add IP'
+          id='test'
+          showTitle={true}
+          title='Whitelisted IPs'
+          values={props.values.test}
+        />
+      )}
+    </Formik>
   ))
   .add('List builder capped', () => (
-    <StringListBuilder
-      addTitle='Add Usergroup'
-      max={8}
-      showTitle={true}
-      title='Usergroups'
-      values={[
-        'test 1',
-        'test 1',
-        'test 1',
-        'test 1',
-        'test 1',
-        'test 1',
-        'test 1'
-      ]}
-    />
+    <Formik
+      initialValues={{
+        test: [
+          'test 1',
+          'test 2',
+          'test 3',
+          'test 4',
+          'test 5',
+          'test 6',
+          'test 7'
+        ]
+      }}
+      validate={action('validate')}
+      onSubmit={action('submit')}
+    >
+      {props => (
+        <StringListBuilder
+          addTitle='Add Usergroup'
+          id='test'
+          max={8}
+          showTitle={true}
+          title='Usergroups'
+          values={props.values.test}
+        />
+      )}
+    </Formik>
   ));
