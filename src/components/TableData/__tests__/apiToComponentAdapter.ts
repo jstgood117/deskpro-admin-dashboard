4416132b99e8e8d12c5e6b3945f2c7808a1eb84c
pxzdev <fortunewillsmileonme117@gmail.com>
2020-01-26 18:15:20 +0300

    feature/8135-date-column-type
    Make output match sketch. Add test file

---
+++ src/components/TableData/__tests__/apiToComponentAdapter.ts
@@ -0,0 +1,18 @@
+import { generateComponentProps } from '../apiToComponentAdapter';
+
+describe('apiToComponentAdapater tests', () => {
+  describe('generateComponentProps tests', () => {
+    it('should return formatted date value', () =>
+      expect(
+        generateComponentProps({
+          column: {
+            type: {
+              __typename: 'TableColumnDateTime',
+              value: { dataPath: 'date_created' }
+            }
+          },
+          row: { original: { date_created: '2019-06-25 19:24:04' } }
+        })
+      ).toEqual({ props: { date_time: 'June 25, 2019' }, type: 'date_time' }));
+  });
+});
