21374b8e436871376f48e3e97bc3e292f76c59fb
alex-mazuruk <alexander.mazuruk.upwork@gmail.com>
2020-01-27 15:36:58 +0100

    Make title and tooltip vertical align center

--- src/components/SettingsForm/components/helpers/generateTitleAndDescription.tsx
+++ src/components/SettingsForm/components/helpers/generateTitleAndDescription.tsx
@@ -1,14 +1,36 @@
 import React from 'react';
+import Tooltip from '../../../Tooltip';
+import Icon from '../../../Icon';
 
 export const generateTitleAndDescription = (className: string, props: any) => {
 
-  if(!props.title && !props.description) {
+  if (!props.title && !props.description) {
     return null;
   }
 
   return (
     <div className='element-details'>
-      {props.title && <label>{props.title}</label>}
+      <div className='title'>
+        {props.title && <label>{props.title}</label>}
+        {props.tooltip && (
+          <Tooltip
+            content={props.tooltip}
+            styleType='light'
+            placement='bottom-start'
+            distance={0}
+          >
+            <span
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                paddingLeft: '4px'
+              }}
+            >
+              <Icon name='info-question-text' />
+            </span>
+          </Tooltip>
+        )}
+      </div>
       {props.description && <p>{props.description}</p>}
     </div>
   );
