4400c50bab79f9bf2e637e86e98f9049802c5394
pxzdev <fortunewillsmileonme117@gmail.com>
2020-01-27 03:16:11 +0300

    feature/7864-async-link-actions
    Add async selected options for actions

--- src/services/actions/run.ts
+++ src/services/actions/run.ts
@@ -1,13 +1,15 @@
 import { ActionsType } from './types';
 import { ApolloClient } from 'apollo-client';
+import { DocumentNode } from 'graphql';
+import { API_AgentTeam } from '../../codegen/types';
+import { IOptions } from '../../types';
 
 export const runAction = async (
   client: ApolloClient<any>,
   action: ActionsType,
   variables: any
 ) => {
-  switch(action.type) {
-
+  switch (action.type) {
     case 'mutate':
       return client.mutate({
         mutation: action.schema,
@@ -16,4 +18,28 @@ export const runAction = async (
     default:
       return undefined;
   }
+};
+
+/**
+ * Returns fetched gql query in IOptions[] format
+ *
+ * @param client
+ * @param query
+ */
+export const querySelectOptions = async (
+  client: ApolloClient<any>,
+  query: DocumentNode
+): Promise<IOptions[]> => {
+  const result = await client.query({ query });
+  return (
+    result.data &&
+    Array.isArray(result.data.agents_teams_getTeams) &&
+    result.data.agents_teams_getTeams.map(
+      (team: API_AgentTeam): IOptions => ({
+        label: team.name,
+        value: team.id,
+        image: team.avatarUrn
+      })
+    )
+  );
 };
