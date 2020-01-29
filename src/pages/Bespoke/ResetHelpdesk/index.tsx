4385e4058440a3051ccff9a7da634bf94dcee45b
pxzdev <fortunewillsmileonme117@gmail.com>
2020-01-29 00:35:13 -0400

    configure route page

---
+++ src/pages/Bespoke/ResetHelpdesk/index.tsx
@@ -0,0 +1,16 @@
+import React, { FC } from 'react';
+
+interface IProps {
+  path: string;
+}
+
+const ResetHelpdeskPage: FC<IProps> = () => {
+
+  return (
+    <div>
+      <p>Reset Helpdesk page</p>
+    </div>
+  );
+};
+
+export default ResetHelpdeskPage;
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,11 +1,10 @@
-
 import AgentsSettings from './Agents/Settings';
 import ExamplePage from './Bespoke/Example';
+import ResetHelpdesk from './Bespoke/ResetHelpdesk';
 import { KeyValue } from '../types';
-
 
 export const RouteToPage: KeyValue = {
   '/agents/settings': AgentsSettings,
   '/sysadmin/settings': ExamplePage,
+  '/help-center/kb/settings': ResetHelpdesk
 };
-
