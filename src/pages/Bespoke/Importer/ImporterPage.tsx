0b96987c637d03a7efc415eccff5c0606b795cbb
jstgood117 <jstgood117@gmail.com>
2020-01-29 19:10:18 +0100

    Created page

---
+++ src/pages/Bespoke/Importer/ImporterPage.tsx
@@ -0,0 +1,16 @@
+import React, { FC } from 'react';
+
+interface IProps {
+  path: string;
+}
+
+const ImporterPage: FC<IProps> = () => {
+
+  return (
+    <div>
+      <p>Importer page</p>
+    </div>
+  );
+};
+
+export default ImporterPage;
