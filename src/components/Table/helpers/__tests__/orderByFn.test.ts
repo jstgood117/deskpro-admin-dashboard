c2fa11b7aefdfa2c0ee16ba03e26ed8d5aaee9ab
Richard Fernandez <richardhealy81@gmail.com>
2020-02-04 13:38:05 +0800

    Fixed data for hierarachy sorting test

--- src/components/Table/helpers/__tests__/orderByFn.test.ts
+++ src/components/Table/helpers/__tests__/orderByFn.test.ts
@@ -3,7 +3,43 @@ import data from '../../../../fixtures/largeData';
 import { customSortMethod  } from '../../../../utils/sort';
 
 describe('orderByFn', () => {
-  test('performance test', () => {
-    orderByFn(data, [customSortMethod], [true]);
+  xtest('performance test - 1000 rows runs in under 200ms', () => {
+
+    const t0 = performance.now();
+    orderByFn(data, [customSortMethod], [false]);
+    const t1 = performance.now();
+    expect(t1-t0).toBeLessThan(200);
+  });
+
+  test('sorts rows correctly in ascending order', () => {
+
+    const result = orderByFn(data, [customSortMethod], [false]);
+
+    const ids = result.map(_item => _item.id);
+    let sorted = true;
+    for (let i = 0; i < ids.length - 1; i++) {
+      if (ids[i] > ids[i+1]) {
+        sorted = false;
+        break;
+      }
+    }
+
+    expect(sorted).toBe(true);
+  });
+
+  test('sorts rows correctly in descending order', () => {
+
+    const result = orderByFn(data, [customSortMethod], [true]);
+
+    const ids = result.map(_item => _item.id);
+    let sorted = true;
+    for (let i = 0; i < ids.length - 1; i++) {
+      if (ids[i] < ids[i+1]) {
+        sorted = false;
+        break;
+      }
+    }
+
+    expect(sorted).toBe(true);
   });
 });
