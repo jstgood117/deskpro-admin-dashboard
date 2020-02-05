a1e5411c7c9094c7d0ee8247bb782d3aee255a33
alex-mazuruk <alexander.mazuruk.upwork@gmail.com>
2020-02-05 05:05:03 +0100

    Add router for data-center

---
+++ src/pages/Bespoke/DataCenter/index.tsx
@@ -0,0 +1,16 @@
+import React, { FC } from 'react';
+
+interface IProps {
+  path: string;
+}
+
+const DataCenter: FC<IProps> = () => {
+
+  return (
+    <div>
+      <p>Data Center</p>
+    </div>
+  );
+};
+
+export default DataCenter;
