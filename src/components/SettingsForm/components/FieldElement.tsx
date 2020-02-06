11ed96b8846f086912b223bfe7dd0eddf424c546
alex-mazuruk <alexander.mazuruk.upwork@gmail.com>
2020-02-06 10:01:55 +0100

    Add button as field type

--- src/components/SettingsForm/components/FieldElement.tsx
+++ src/components/SettingsForm/components/FieldElement.tsx
@@ -9,6 +9,8 @@ import Profiles from '../../Profiles';
 import StringListBuilder from '../../StringListBuilder';
 import Units from '../../Units';
 import Checkbox from '../../Checkbox';
+import Button from '../../Button';
+import Icon from '../../Icon';
 import SingleSelect from '../../SelectComponents/SingleSelect';
 import { UnitsValuesType } from '../../Units/Units';
 
@@ -79,6 +81,16 @@ const elementsSelector: {
       placeholder={props.placeholder}
       type='primary'
     />
+  ),
+  button: props => (
+    <Button
+      styleType={props.styleType ? props.styleType : 'secondary'}
+      onClick={() => { }}
+      size='small'
+    >
+      {props.icon && <Icon name={props.icon} />}
+      {props.text}
+    </Button>
   ),
   profiles: props => (
     <Profiles
