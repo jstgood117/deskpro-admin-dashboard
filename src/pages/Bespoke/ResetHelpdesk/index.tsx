4b2e40f515e2111aed6ccac5b8a5c9cb2b99463b
pxzdev <fortunewillsmileonme117@gmail.com>
2020-02-06 11:27:04 -0400

    fix label font-weight

--- src/pages/Bespoke/ResetHelpdesk/index.tsx
+++ src/pages/Bespoke/ResetHelpdesk/index.tsx
@@ -76,7 +76,7 @@ const Container = styled.div`
       label {
         font-family: Rubik;
         font-style: normal;
-        font-weight: 500;
+        font-weight: normal;
         font-size: 14px;
         line-height: 150%;
         color: #4c4f50;
