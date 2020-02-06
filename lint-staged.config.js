e9888f9314903414ef3b7330d89e5f757686e3f6
Richard Fernandez <richardhealy81@gmail.com>
2020-02-06 18:12:47 +0800

    Fixing lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     'src/**/*.{js,jsx,ts,tsx}': files => {
         return files
-            .filter(f => !f.indexOf('codegen') === -1)
+            .filter(f => f.indexOf('codegen') === -1)
             .map(f => `npm run lint ${f}`);
     }
 }
