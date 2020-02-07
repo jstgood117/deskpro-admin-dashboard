9fbc689762b66cfabecdd9183355312ea8b2ad72
alex-mazuruk <alexander.mazuruk.upwork@gmail.com>
2020-02-07 20:12:23 +0100

    Update uiSchema

--- src/pages/Bespoke/TicketDeflection/testData.ts
+++ src/pages/Bespoke/TicketDeflection/testData.ts
@@ -7,29 +7,19 @@ export const uiSchema: {
     {
       title: 'Ticket Deflection',
       type: 'feature_section',
-      elements: [
-        {
-          title: '',
-          type: 'page_section',
-          header: [
-            {
-              title: 'Ticket Deflection',
-              showOn: 'ticket_feflection_enabled',
-              description:
-                'Use existing Knowledgebase and Guide content to deflect new tickets by showing users articles that relate to the ticket they are submitting.',
-              info: [
-                {
-                  type: 'button',
-                  title: 'Ticket Deflection',
-                  url: 'http://www.test.com',
-                  icon: 'guide',
-                },
-              ],
-            },
-          ],
-          elements: [],
+      header: {
+        title: 'Ticket Deflection',
+        showOn: 'ticket_feflection_enabled',
+        description:
+          'Use existing Knowledgebase and Guide content to deflect new tickets by showing users articles that relate to the ticket they are submitting.',
+        info: {
+          type: 'button',
+          title: 'Ticket Deflection',
+          url: 'http://www.test.com',
+          icon: 'guide',
         },
-      ],
+      },
+      elements: [],
     },
   ],
 };
