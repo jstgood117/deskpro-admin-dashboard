15a9f63a7f511516cf6c7db19c668d0e5b7e377d
pxzdev <fortunewillsmileonme117@gmail.com>
2020-02-11 15:31:48 -0400

    add settings data helper to storybook

--- src/components/SettingsData/stories.tsx
+++ src/components/SettingsData/stories.tsx
@@ -3,10 +3,17 @@ import { storiesOf } from '@storybook/react';
 
 import SettingsData from './SettingsData';
 
+import { SettingsData as SettingsDataHelper } from './Helpers';
+
 storiesOf('Settings Data', module)
   .add('header-card', () => <SettingsData type='header-card' />)
   .add('header-medium-card', () => <SettingsData type='header-medium-card' />)
   .add('setting-info', () => <SettingsData type='setting-info' />)
   .add('setting-info-inUse', () => <SettingsData type='setting-info-inUse' />)
   .add('feature-billing', () => <SettingsData type='feature-billing' />)
-  .add('reference-code-panel', () => <SettingsData type='reference-code-panel' />);
+  .add('reference-code-panel', () => (
+    <SettingsData type='reference-code-panel' />
+  ))
+  .add('settings data helper', () => (
+    <SettingsDataHelper title='title' description='desc' />
+  ));
