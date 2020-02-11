710be8496eba57537b7fdcf12da6a84fe486ae7d
pxzdev <fortunewillsmileonme117@gmail.com>
2020-02-11 16:34:02 -0400

    add settings data tests

--- src/components/SettingsData/SettingsData.test.tsx
+++ src/components/SettingsData/SettingsData.test.tsx
@@ -16,15 +16,81 @@ describe('SettingsData', () => {
     return mountedSettingsData;
   };
 
-  beforeEach(() => {
-    props = {
-      type: 'header-card'
-    };
-    mountedSettingsData = undefined;
+  describe('SettingsData: header-card', () => {
+    beforeEach(() => {
+      props = {
+        type: 'header-card'
+      };
+      mountedSettingsData = undefined;
+    });
+    it('always renders a <div>', () => {
+      const elts = wrapper(false).find('div');
+      expect(elts.length).toBeGreaterThan(0);
+    });
   });
 
-  it('always renders a <div>', () => {
-    const elts = wrapper(false).find('div');
-    expect(elts.length).toBeGreaterThan(0);
+  describe('SettingsData: header-medium-card', () => {
+    beforeEach(() => {
+      props = {
+        type: 'header-medium-card'
+      };
+      mountedSettingsData = undefined;
+    });
+    it('always renders a <div>', () => {
+      const elts = wrapper(false).find('div');
+      expect(elts.length).toBeGreaterThan(0);
+    });
+  });
+
+  describe('SettingsData: setting-info', () => {
+    beforeEach(() => {
+      props = {
+        type: 'setting-info'
+      };
+      mountedSettingsData = undefined;
+    });
+    it('always renders a <div>', () => {
+      const elts = wrapper(false).find('div');
+      expect(elts.length).toBeGreaterThan(0);
+    });
+  });
+
+  describe('SettingsData: setting-info-inUse', () => {
+    beforeEach(() => {
+      props = {
+        type: 'setting-info-inUse'
+      };
+      mountedSettingsData = undefined;
+    });
+    it('always renders a <div>', () => {
+      const elts = wrapper(false).find('div');
+      expect(elts.length).toBeGreaterThan(0);
+    });
+  });
+
+  describe('SettingsData: feature-billing', () => {
+    beforeEach(() => {
+      props = {
+        type: 'feature-billing'
+      };
+      mountedSettingsData = undefined;
+    });
+    it('always renders a <div>', () => {
+      const elts = wrapper(false).find('div');
+      expect(elts.length).toBeGreaterThan(0);
+    });
+  });
+
+  describe('SettingsData: reference-code-panel', () => {
+    beforeEach(() => {
+      props = {
+        type: 'reference-code-panel'
+      };
+      mountedSettingsData = undefined;
+    });
+    it('always renders a <div>', () => {
+      const elts = wrapper(false).find('div');
+      expect(elts.length).toBeGreaterThan(0);
+    });
   });
 });
