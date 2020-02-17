fa3eec7a6506a9210602d3e34660b55f421824a7
pxzdev <fortunewillsmileonme117@gmail.com>
2020-02-17 14:07:38 +0700

    Implement Generatl Settings page - Adding Unittest file

---
+++ src/components/SettingsForm/components/InlineEdit.test.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import {mount} from '../../../test/enzyme';
+
+import InlineEdit from './InlineEdit';
+
+describe('InlineEdit', () => {
+  const wrapper = ({ inline, field, formikProps }: { inline?: any, field?: any, formikProps?: any }) =>
+    mount(
+      <InlineEdit
+        inline={inline}
+        field={field}
+        formikProps={formikProps}
+      />
+    );
+  it('should render', () => {
+    const root = wrapper({
+      inline: [
+        {
+          type: 'label',
+          label: 'Require this many symbols: '
+        },
+        {
+          type: 'input',
+          id: 'custom_policy_require_symbols_value',
+        }
+      ],
+      field: {
+        type: 'checkbox',
+        id: 'custom_policy_require_symbols',
+        value: 'yes'
+      },
+      formikProps: {
+        values: {
+          custom_policy_require_symbols_value: 1,
+          custom_policy_require_symbols: 'yes'
+        }
+      }
+    });
+    expect(root.length).toBeGreaterThan(0);
+  });
+});
