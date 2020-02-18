028b8161f5f555caf8e864403f9761be7e51bb1d
alex-mazuruk <alexander.mazuruk.upwork@gmail.com>
2020-02-18 08:26:05 +0100

    Add StringListbuilder to storybook

---
+++ src/components/StringListBuilderNew/stories.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { Formik } from 'formik';
+
+import StringListBuilder from './StringListBuilder';
+
+const options = [
+  'Usergroup 5',
+  'Usergroup 6',
+  'Extra Privileged',
+  'General Users',
+  'Registered',
+  'VIP',
+  'Usergroup 7',
+  'Usergroup 8',
+];
+
+const selectedOptions = [
+  'Extra Privileged',
+  'General Users',
+  'Registered',
+  'VIP',
+];
+
+storiesOf('String List Builder New', module)
+  .add('List builder usergroups', () => (
+    <Formik
+      initialValues={{
+        options,
+        selectedOptions,
+      }}
+      validate={action('validate')}
+      onSubmit={action('submit')}
+    >
+      {props => (
+        <StringListBuilder
+          id='selectedOptions'
+          title='Usergroups'
+          buttonTitle='Add Usergroup'
+          options={props.values.options}
+          selectedOptions={props.values['selectedOptions']}
+          tooltip='All usergroups have been added.'
+          formikProps={props}
+        />
+      )}
+    </Formik>
+  ));
