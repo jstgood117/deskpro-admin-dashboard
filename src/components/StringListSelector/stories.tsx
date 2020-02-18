3bfd612bc4c1d80e94556112ddc1638cf021fca5
alex-mazuruk <alexander.mazuruk.upwork@gmail.com>
2020-02-18 08:45:27 +0100

    Add StringListSelector to storybook

---
+++ src/components/StringListSelector/stories.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { storiesOf } from '@storybook/react';
+
+import StringListSelector from './StringListSelector';
+
+const values = [
+  { id: 0, value: 'Extra Privileged', selected: true },
+  { id: 1, value: 'General Users', selected: true },
+  { id: 2, value: 'Registered', selected: true },
+  { id: 3, value: 'VIP', selected: true },
+  { id: 4, value: 'Usergroup 5', selected: false },
+  { id: 5, value: 'Usergroup 6', selected: false },
+  { id: 6, value: 'Usergroup 7', selected: false },
+  { id: 7, value: 'Usergroup 8', selected: false },
+];
+
+storiesOf('StringListSelector', module)
+  .add('default', () => {
+    const [items, setItems] = useState(values);
+
+    return (
+      <StringListSelector
+        description=''
+        items={items}
+        onSelect={newItems => setItems(newItems)}
+      />
+    );
+  });
