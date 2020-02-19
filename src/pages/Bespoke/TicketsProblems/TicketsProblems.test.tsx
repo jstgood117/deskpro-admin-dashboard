cb27740d53dff09fd274fab22bde8cfd3c518dbd
alex-mazuruk <alexander.mazuruk.upwork@gmail.com>
2020-02-19 20:16:07 +0100

    Fix profiles validation issue

--- src/pages/Bespoke/TicketsProblems/TicketsProblems.test.tsx
+++ src/pages/Bespoke/TicketsProblems/TicketsProblems.test.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { mount, shallow } from 'enzyme';
 import TicketsProblemsPage from './index';
+
+jest.mock('react-dom', () => ({
+  createPortal: (node: ReactNode) => node
+}));
 
 describe('TicketsProblemsPage', () => {
   let mountedPage: any;
@@ -14,8 +18,8 @@ describe('TicketsProblemsPage', () => {
     return mountedPage;
   };
 
-  it('always renders a <div>', () => {
-    const elts = wrapper(false).find('div');
-    expect(elts.length).toBeGreaterThan(0);
+  it('renders a <div> root', () => {
+    const root = wrapper(true);
+    expect(root.length).toEqual(1);
   });
 });
