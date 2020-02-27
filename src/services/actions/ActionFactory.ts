a9cdb058954b6873fdc440a5a07b4741ac2d3733
pxzdev <fortunewillsmileonme117@gmail.com>
2020-02-27 16:42:57 +0700

    Update unittest

--- src/services/actions/ActionFactory.ts
+++ src/services/actions/ActionFactory.ts
@@ -1,12 +1,11 @@
 import agents from './pathActions/agents';
-import ticketForms from './pathActions/ticketForms';
 
 export const ActionFactory = (path: string) => {
   switch (path) {
     case '/agents':
       return agents;
     case '/tickets/forms':
-      return ticketForms;
+      return agents;
     default:
       return null;
   }
