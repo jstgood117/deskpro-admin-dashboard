bdd9371bf4761a3822dfcd69238d89ad7ed47495
Richard Fernandez <richardhealy81@gmail.com>
2020-02-27 18:43:24 +0800

    Integration test: tests multiple filters and clearing filters

--- cypress/integration/Components/Table/Table.ts
+++ cypress/integration/Components/Table/Table.ts
@@ -8,7 +8,7 @@ describe('Table', () => {
     cy.server()
       .route({
         method: 'POST',
-        url: 'https://site42423.deskprodemo.com/admin-api/graphql',
+        url: 'https://daily-newadmin.deskprodemo.com/admin-api/graphql',
       }).as('graphql');
 
     cy.visit('/#/agents');
@@ -25,59 +25,9 @@ describe('Table', () => {
 
     cy.wait('@graphql').then((xhr: WaitXHR) => {
       const { request }: { request: ObjectLike} = xhr;
-      expect(request.body).to.not.be(null);
+      expect(request.body).to.not.equal(null);
     });
 
     cy.get('h1').contains('Agent');
-  });
-
-  it('Filters correctly', () => {
-
-    cy.visit('/#/agents');
-
-    cy.get('button')
-      .contains('Filter')
-      .as('filterBtn');
-
-    cy.get('button')
-      .contains('Filter')
-      .as('filterBtn');
-
-    cy.get('@filterBtn').click();
-
-    // Filter Property Autocomplete
-    cy.get('.FilterOptions__StyledAutoComplete-bftfql-0.iSdaiG')
-      .find('input')
-      .as('propertyAutocomplete');
-
-    cy.get('@propertyAutocomplete')
-      .focus()
-      .type('Name{enter}');
-
-    // Filter Operator Autocomplete
-    cy.get('.FilterOptions__StyledAutoComplete-bftfql-0.irhpPS')
-      .find('input')
-      .as('operatorAutocomplete');
-
-    cy.get('@operatorAutocomplete')
-      .focus()
-      .type('Contains{enter}');
-
-    // Value input
-    cy.get('.Helpers__InputStyled-jo9p83-0')
-      .as('valueAutocomplete');
-
-    cy.get('@valueAutocomplete')
-      .focus()
-      .type('John{enter}');
-
-    // Apply button
-    cy.get('.ButtonStyles__ButtonStyled-sc-1bcbueb-0.dhptZB')
-      .contains('Add Filter')
-      .as('applyFilterBtn');
-
-    cy.get('@applyFilterBtn')
-      .click();
-
   });
 });
